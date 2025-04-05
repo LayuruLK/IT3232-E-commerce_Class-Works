2a55b7242daaaf8526c620c3b3c7d54795795b1e
Layuru Karunathilaka <139636618+LayuruLK@users.noreply.github.com>
2025-04-05 10:59:19 +0530

    Add files via upload

---
+++ Day 03/src/main/java/lk/ac/vau/fas/ict/first/FirstApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.vau.fas.ict.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApplication.class, args);
+	}
+
+}
