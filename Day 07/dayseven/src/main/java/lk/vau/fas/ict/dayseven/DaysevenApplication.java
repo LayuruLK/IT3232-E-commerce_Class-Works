9c96fd1f99eff26252f48fcb8483c1c289e13a4a
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2025-04-26 15:24:54 +0530

    chore:initialized day 7 project

---
+++ Day 07/dayseven/src/main/java/lk/vau/fas/ict/dayseven/DaysevenApplication.java
@@ -0,0 +1,13 @@
+package lk.vau.fas.ict.dayseven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaysevenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaysevenApplication.class, args);
+	}
+
+}
---
+++ Day 07/dayseven/src/test/java/lk/vau/fas/ict/dayseven/DaysevenApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.vau.fas.ict.dayseven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaysevenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
