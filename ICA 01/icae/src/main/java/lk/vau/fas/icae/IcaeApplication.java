bbc670fbffc2c8fac6a8fa49bb69157848e16220
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2025-05-05 09:49:00 +0530

    chore:project initialized

---
+++ ICA 01/icae/src/main/java/lk/vau/fas/icae/IcaeApplication.java
@@ -0,0 +1,13 @@
+package lk.vau.fas.icae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcaeApplication.class, args);
+	}
+
+}
