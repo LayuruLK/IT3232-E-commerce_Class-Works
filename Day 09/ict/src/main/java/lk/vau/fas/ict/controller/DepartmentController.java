31ef1169ee3f11f7c1f97a315ec93c9153151f73
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2025-05-17 23:28:09 +0530

    feat: add DepartmentController to handle department-related endpoints

--- Day 09/ict/src/main/java/lk/vau/fas/ict/controller/DepartmentController.java
+++ Day 09/ict/src/main/java/lk/vau/fas/ict/controller/DepartmentController.java
@@ -1,10 +1,25 @@
 package lk.vau.fas.ict.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lk.vau.fas.ict.model.Department;
+import lk.vau.fas.ict.service.DepartmentService;
 
 @RestController
 @RequestMapping("/dept")
 public class DepartmentController {
     
+    @Autowired
+    public DepartmentService service;
+
+    public ResponseEntity<List<Department>> getAllDepartments() {
+        return new ResponseEntity<List<Department>>
+        (service.getAllDepartments(),HttpStatus.OK);
+    }
 }
