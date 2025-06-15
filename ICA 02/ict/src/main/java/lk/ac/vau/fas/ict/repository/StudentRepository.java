a18c3eb18127045fd7ea03ea688a184e592b73d7
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2025-06-16 00:04:40 +0530

    feat:created repository for fetch students who borros book for given book ID

---
+++ ICA 02/ict/src/main/java/lk/ac/vau/fas/ict/repository/StudentRepository.java
@@ -0,0 +1,17 @@
+package lk.ac.vau.fas.ict.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import lk.ac.vau.fas.ict.models.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, String> {
+    
+    // Get student who borrowed books when given book id
+    @Query("SELECT s from Student s JOIN b.borrows WHERE b.book.id=?1")
+    public List<Student> getBorrowedStudent(String bookId);
+}
