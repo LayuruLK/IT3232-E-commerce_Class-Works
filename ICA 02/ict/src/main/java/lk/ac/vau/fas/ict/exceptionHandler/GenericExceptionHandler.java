dcea6b842e6c74ee4525a2d7ecc2e1984e7fa19c
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2025-06-16 22:34:49 +0530

    feat:create new exception handler for CONFLICT

--- ICA 02/ict/src/main/java/lk/ac/vau/fas/ict/exceptionHandler/GenericExceptionHandler.java
+++ ICA 02/ict/src/main/java/lk/ac/vau/fas/ict/exceptionHandler/GenericExceptionHandler.java
@@ -1,5 +1,6 @@
 package lk.ac.vau.fas.ict.exceptionHandler;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +14,12 @@ public class GenericExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorResponce> handleEntityNotFound(EntityNotFoundException exception) {
+        ErrorResponce errorResponce = new ErrorResponce(HttpStatus.NOT_FOUND.value(),exception.getMessage().toString());
+        return new ResponseEntity<ErrorResponce>(errorResponce,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponce> handleEntityNotFound(DataIntegrityViolationException exception) {
         ErrorResponce errorResponce = new ErrorResponce(HttpStatus.NOT_FOUND.value(),exception.getMessage().toString());
         return new ResponseEntity<ErrorResponce>(errorResponce,HttpStatus.NOT_FOUND);
     }
